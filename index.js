99de9b2009511094c935b8529595b3e6b4034b68
Mauro Vargas <maurovargas7725@gmail.com>
2019-12-27 18:20:17 -0700

    init2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const { pool } = require('./config')
-
+//
 const app = express()
 
 app.use(bodyParser.json())
