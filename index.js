8d9f84c9d55e9621dced90214840748b5ed1f38a
Mauro Vargas <maurovargas7725@gmail.com>
2019-12-29 14:07:59 -0700

    added models: hemet, compression, rateLimit, body, check

--- index.js
+++ index.js
@@ -2,16 +2,75 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { pool } = require('./config')
 const app = express()
+const helmet = require('helmet')
+const compression = require('compression')
+const rateLimit = require('express-rate-limit')
+const { body, check } = require('express-validator')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
+app.use(compression())
+app.use(helmet())
 
 const isProduction = process.env.NODE_ENV === 'production'
 const origin = {
   origin: isProduction ? 'https://safe-waters-27889.herokuapp.com/' : '*',
 }
 
+app.use(cors(origin))
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 5 // 5 requests,
+})
+
+app.use(limiter)
+
+const postLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 1
+})
+
+app.post('/books', postLimiter, addBook)
+app.post(
+  '/books',
+  [
+    check('author')
+      .not()
+      .isEmpty()
+      .isLength({ min: 5, max: 255 })
+      .trim(),
+    check('title')
+      .not()
+      .isEmpty()
+      .isLength({ min: 5, max: 255 })
+      .trim(),
+  ],
+  postLimiter,
+  (request, response) => {
+    const errors = validationResult(request)
+
+    if (!errors.isEmpty()) {
+      return response.status(422).json({ errors: errors.array() })
+    }
+
+    const { author, title } = request.body
+
+    pool.query('INSERT INTO books (author, title) VALUES ($1, $2)', [author, title], error => {
+      if (error) {
+        throw error
+      }
+      response.status(201).json({ status: 'success', message: 'Book added.' })
+    })
+  }
+)
+
+
+const deleteBook = (request, response) => {
+  if (!request.header('apiKey') || request.header('apiKey') !== process.env.API_KEY) {
+    return response.status(401).json({ status: 'error', message: 'Unauthorized.' })
+  }
+}
 
 const getBooks = (request, response) => {
   pool.query('SELECT * FROM books', (error, results) => {
