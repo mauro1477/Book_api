c1a625796ff7f3424e51df0c206e3a84d432581e
Mauro Vargas <maurovargas7725@gmail.com>
2019-12-29 13:51:55 -0700

    update4

--- index.js
+++ index.js
@@ -16,68 +16,68 @@ const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
+//app.use(cors())
 // app.use(cors(origin))////////////////////////////CORS IS USED TO PREVENT HIJACKS
 // app.use(limiter)
 // app.use(compression())
 // app.use(helmet({
 //   frameguard: false
 // }))
-app.post('/books', postLimiter, addBook)
-
-app.post(
-  '/books',
-  [
-    check('author')
-      .not()
-      .isEmpty()
-      .isLength({ min: 5, max: 255 })
-      .trim(),
-    check('title')
-      .not()
-      .isEmpty()
-      .isLength({ min: 5, max: 255 })
-      .trim(),
-  ],
-  postLimiter,
-  (request, response) => {
-    const errors = validationResult(request)
-
-    if (!errors.isEmpty()) {
-      return response.status(422).json({ errors: errors.array() })
-    }
-
-    const { author, title } = request.body
-
-    pool.query('INSERT INTO books (author, title) VALUES ($1, $2)', [author, title], error => {
-      if (error) {
-        throw error
-      }
-      response.status(201).json({ status: 'success', message: 'Book added.' })
-    })
-  }
-)
+// app.post('/books', postLimiter, addBook)
+//
+// app.post(
+//   '/books',
+//   [
+//     check('author')
+//       .not()
+//       .isEmpty()
+//       .isLength({ min: 5, max: 255 })
+//       .trim(),
+//     check('title')
+//       .not()
+//       .isEmpty()
+//       .isLength({ min: 5, max: 255 })
+//       .trim(),
+//   ],
+//   postLimiter,
+//   (request, response) => {
+//     const errors = validationResult(request)
+//
+//     if (!errors.isEmpty()) {
+//       return response.status(422).json({ errors: errors.array() })
+//     }
+//
+//     const { author, title } = request.body
+//
+//     pool.query('INSERT INTO books (author, title) VALUES ($1, $2)', [author, title], error => {
+//       if (error) {
+//         throw error
+//       }
+//       response.status(201).json({ status: 'success', message: 'Book added.' })
+//     })
+//   }
+// )
 
 const isProduction = process.env.NODE_ENV === 'production'
 const origin = {
   origin: isProduction ? 'https://safe-waters-27889.herokuapp.com/' : '*',
 }
 
-const postLimiter = rateLimit({
-  windowMs: 1 * 60 * 1000,
-  max: 1
-})
+// const postLimiter = rateLimit({
+//   windowMs: 1 * 60 * 1000,
+//   max: 1
+// })
+//
+// const limiter = rateLimit({
+//   windowMs: 1 * 60 * 1000, // 1 minute
+//   max: 5 // 5 requests,
+// })
 
-const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minute
-  max: 5 // 5 requests,
-})
-
-const deleteBook = (request, response) => {
-  if (!request.header('apiKey') || request.header('apiKey') !== process.env.API_KEY) {
-    return response.status(401).json({ status: 'error', message: 'Unauthorized.' })
-  }
-}
+// const deleteBook = (request, response) => {
+//   if (!request.header('apiKey') || request.header('apiKey') !== process.env.API_KEY) {
+//     return response.status(401).json({ status: 'error', message: 'Unauthorized.' })
+//   }
+// }
 
 const getBooks = (request, response) => {
   pool.query('SELECT * FROM books', (error, results) => {
@@ -106,7 +106,7 @@ app
   .get(getBooks)
   // POST endpoint
   .post(addBook)
-  
+
 // Start server
 app.listen(process.env.PORT || 3002, () => {
   console.log(`Server listening`)
