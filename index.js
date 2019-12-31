66aa87ff22fbfc74c0d74e643491effeff1a4b7e
Mauro Vargas <maurovargas7725@gmail.com>
2019-12-30 22:21:19 -0700

    query error7

--- index.js
+++ index.js
@@ -23,12 +23,11 @@ app.get('/', function(req, res) {
 app.post('/auth', function(request, response) {
   console.log('/auth');
   //const {author, title} = request.body//Make sure these name match to the html page
-  
 	var author_name = request.body.author;
 	var title_name = request.body.title;
 	console.log(author_name);
 	console.log(title_name);
-  pool.query('SELECT * FROM books WHERE author ==  $1 AND title == $2', [author_name, title_name], (error, results) => {
+  pool.query('SELECT * FROM books WHERE author =  $1 AND title = $2', [author_name, title_name], (error, results) => {
     if(error){
       throw error
     }
