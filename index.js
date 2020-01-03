a62304c9ef4537c125c66c25bb36bf1b8e1661ce
Mauro Vargas <maurovargas7725@gmail.com>
2020-01-02 18:09:02 -0700

    query error 16

--- index.js
+++ index.js
@@ -42,24 +42,53 @@ const addBook = (request, response) => {
 //   .post(addBook)
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/views/login.html')
-		console.log(req.action);
-    //console.log(getBooks);
-});
-
-app.post('/auth', function(request, response) {
-  console.log('/auth');
-  //const {author, title} = request.body//Make sure these name match to the html page
-	var author_name = request.body.author;
-	var title_name = request.body.title;
-	console.log(author_name);
-	console.log(title_name);
-  pool.query('SELECT * FROM books WHERE author =  $1 AND title = $2', [author_name, title_name], (error, results) => {
-    if(error){
+  pool.query('SELECT * FROM books', (error, results) => {
+    if (error) {
       throw error
     }
-    	response.status(200).json(results.rows[0])
-      console.log(results.rows[0].title);
+    response.status(200).json(results.rows)
+  })
+});
+
+// app.post('/auth', function(request, response) {
+//   console.log('/auth');
+//   //const {author, title} = request.body//Make sure these name match to the html page
+// 	var author_name = request.body.author;
+// 	var title_name = request.body.title;
+// 	console.log(author_name);
+// 	console.log(title_name);
+//   pool.query('SELECT * FROM books WHERE author =  $1 AND title = $2', [author_name, title_name], (error, results) => {
+//     if(error){
+//       throw error
+//     }
+//     	response.status(200).json(results.rows[0])
+//   })
+// });
+
+app.get('/inventory', function(req,res) {
+  var query = "select * from inventory where ingredient_quantity > 10;";
+  var query1 = "select * from inventory where ingredient_quantity <= 10";
+  db.task('get-everything', task => {
+    return task.batch([
+      task.any(query),
+      task.any(query1)
+    ]);
+  })
+  .then(data => {
+    res.render('inventory.pug', {
+      my_title: "Inventory Page",
+      inventory_item_Full: data[0],
+      inventory_item_AlmostEmpty: data[1]
+
+    })
+  })
+  .catch(error => {
+    console.log("error");
+    res.render('inventory.pug', {
+      my_title: "Inventory Page",
+      inventory_item_Full: "",
+      inventory_item_AlmostEmpty: ""
+    })
   })
 });
 
